0005bf0b2c29d69d36981fee72c969d78fe04bb9
ColinMaxwell2001 <95194573+ColinMaxwell2001@users.noreply.github.com>
2022-09-16 19:08:56 -0400

    finished capitalize method for strings

--- exercises/string-exercises/js/strings.js
+++ exercises/string-exercises/js/strings.js
@@ -7,9 +7,9 @@ class StringFunctions {
      * capitalize('HELLO WORLD') // 'Hello world'
      */
     capitalize(input) { 
-      var input2 ="";
-      input2 = input.charAt(0).toUpperCase() + input.slice(1);
-      return input2; 
+      //var input2 ="";
+      //input2 = input.charAt(0).toUpperCase() + input.slice(1);
+      return input.toUpperCase(); 
     }
     /**
      * Returns a reversed string. Example: "abc" => "cba"
