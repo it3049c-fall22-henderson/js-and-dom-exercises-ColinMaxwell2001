789c7f1fba6a5165b0c33164c3c7b29dd501213e
ColinMaxwell2001 <95194573+ColinMaxwell2001@users.noreply.github.com>
2022-09-16 19:00:44 -0400

    attempt to complete first string function and fix running in browser

--- exercises/string-exercises/js/strings.js
+++ exercises/string-exercises/js/strings.js
@@ -7,7 +7,9 @@ class StringFunctions {
      * capitalize('HELLO WORLD') // 'Hello world'
      */
     capitalize(input) { 
-      return input; 
+      var input2 ="";
+      input2 = input.charAt(0).toUpperCase() + input.slice(1);
+      return input2; 
     }
     /**
      * Returns a reversed string. Example: "abc" => "cba"
